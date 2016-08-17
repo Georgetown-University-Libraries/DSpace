cee1abc9c44031674c44681207ff143b4b1aa51d
Terry Brady <terrywbrady@gmail.com>
2016-08-17 11:22:19 -0700

    x

--- dspace-api/src/main/java/org/dspace/discovery/IndexClient.java
+++ dspace-api/src/main/java/org/dspace/discovery/IndexClient.java
@@ -24,7 +24,9 @@ import org.dspace.services.factory.DSpaceServicesFactory;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Class used to reindex dspace communities/collections/items into discovery
@@ -147,7 +149,7 @@ public class IndexClient {
             // Enable batch mode; we may be indexing a large number of items
             //context.enableBatchMode(true);
             final long startTimeMillis = System.currentTimeMillis();
-            final long count = indexAll(indexer,  ContentServiceFactory.getInstance().getItemService(), context, dso);
+            final long count = indexAll(new ArrayList<DSpaceObject>(), indexer,  ContentServiceFactory.getInstance().getItemService(), context, dso);
             final long seconds = (System.currentTimeMillis() - startTimeMillis ) / 1000;
             log.info("Indexed " + count + " DSpace object" + (count > 1 ? "s" : "") + " in " + seconds + " seconds");
         } else {
@@ -163,26 +165,30 @@ public class IndexClient {
     /**
      * Indexes the given object and all children, if applicable.
      */
-    private static long indexAll(final IndexingService indexingService,
+    private static long indexAll(List<DSpaceObject> dsos, final IndexingService indexingService,
                                  final ItemService itemService,
                                  Context context,
                                  DSpaceObject dso) throws IOException, SearchServiceException, SQLException {
         long count = 0;
 
+        dsos.add(dso);
         indexingService.indexContent(context, dso, true, true);
         count++;
         System.out.println("TBTBa "+count+" "+dso.getHandles().size());
+        
         if (dso.getType() == Constants.COMMUNITY) {
             Community community = (Community) dso;
+            
             //final String communityHandle = community.getHandle();
             for (Community subcommunity : community.getSubcommunities()) {
                 System.out.println("TBTBb "+count+" "+subcommunity.getID());
-                count += indexAll(indexingService, itemService, context, subcommunity);
+                count += indexAll(dsos, indexingService, itemService, context, subcommunity);
             }
             //final Community reloadedCommunity = (Community) HandleServiceFactory.getInstance().getHandleService().resolveToObject(context, communityHandle);
             for (Collection collection : community.getCollections()) {
                 count++;
                 System.out.println("TBTBd "+count+" "+collection.getID());
+                dsos.add(collection);
                 indexingService.indexContent(context, collection, true, true);
                 count += indexItems(indexingService, itemService, context, collection);
             }
