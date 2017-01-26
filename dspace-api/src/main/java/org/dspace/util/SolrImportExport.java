bda605743f2fef17653f0e3a5895f41e5cec3d2b
Terry W Brady <twb27@georgetown.edu>
2017-01-26 10:26:53 -0800

    fix export query

--- dspace-api/src/main/java/org/dspace/util/SolrImportExport.java
+++ dspace-api/src/main/java/org/dspace/util/SolrImportExport.java
@@ -578,10 +578,9 @@ public class SolrImportExport
 
         List<String> multivaluedFields = getMultiValuedFields(solr);
         for (String mvField : multivaluedFields) {
-            query.setParam("f." + mvField + ".split", "true");
-            query.setParam("f." + mvField + ".separator", MULTIPLE_VALUES_SPLITTER);
+            query.add("f." + mvField + ".split", "true");
+            query.add("f." + mvField + ".separator", MULTIPLE_VALUES_SPLITTER);
         }
-
 		
 		List<RangeFacet.Count> monthFacets = solr.query(query).getFacetRanges().get(0).getCounts();
 
