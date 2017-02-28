a37982b23f277772c890a5ac949499b68a816178
Terry W Brady <twb27@georgetown.edu>
2017-02-28 13:48:43 -0800

    x

---
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/model/hateoas/InfoResource.java
@@ -0,0 +1,25 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.app.rest.model.hateoas;
+
+import org.dspace.app.rest.model.RestInfo;
+import org.dspace.app.rest.utils.Utils;
+
+/**
+ * Item Rest HAL Resource. The HAL Resource wraps the REST Resource
+ * adding support for the links and embedded resources
+ * 
+ * @author Andrea Bollini (andrea.bollini at 4science.it)
+ *
+ */
+@RelNameDSpaceResource(RestInfo.NAME)
+public class InfoResource extends DSpaceResource<ItemRest> {
+	public InfoResource(RestInfo info, Utils utils, String... rels) {
+		super(info, utils, rels);
+	}
+}
