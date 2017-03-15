d3026d5c1a1266822d9ff6c94c2dc56ecddc94b6
Terry W Brady <twb27@georgetown.edu>
2017-03-15 16:56:55 -0700

    add data to object

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/model/hateoas/InfoResource.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/model/hateoas/InfoResource.java
@@ -4,5 +4,8 @@ import org.springframework.hateoas.ResourceSupport;
 
 @RelNameDSpaceResource("info")
 public class InfoResource extends ResourceSupport {
-
+	private String data = "info";
+	public String getData() {
+		return data;
+	}
 }
