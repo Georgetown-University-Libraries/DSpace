65b32e1bc7384f6d34c0afd8d32d3267da4a1d20
Terry W Brady <twb27@georgetown.edu>
2017-03-15 17:33:56 -0700

    create link

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/model/hateoas/InfoResource.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/model/hateoas/InfoResource.java
@@ -1,11 +1,18 @@
 package org.dspace.app.rest.model.hateoas;
 
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.ResourceSupport;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RelNameDSpaceResource("info")
 public class InfoResource extends ResourceSupport {
 	private String data = "info";
+	
 	public String getData() {
 		return data;
+	}
+
+	public Link getItems() {
+		return linkTo(String.class, "/api/core/items").withRel("");
 	}
 }
