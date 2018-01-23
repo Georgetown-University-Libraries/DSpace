06cbaec9cea52b2f8753bdf15c428bc9c4b98cfd
Terry W Brady <twb27@georgetown.edu>
2018-01-23 10:51:50 -0800

    debug

--- dspace-spring-rest/src/main/java/org/dspace/app/rest/security/EPersonRestAuthenticationProvider.java
+++ dspace-spring-rest/src/main/java/org/dspace/app/rest/security/EPersonRestAuthenticationProvider.java
@@ -130,7 +130,9 @@ public class EPersonRestAuthenticationProvider implements AuthenticationProvider
 
         } else {
             log.info(LogManager.getHeader(context, "failed_login", "No eperson with an non-blank e-mail address found"));
-            throw new BadCredentialsException("Login failed");
+            Exception e = new BadCredentialsException("Login failed");
+            log.error("TBTB Login Failed", e);
+            throw e;
         }
     }
 
